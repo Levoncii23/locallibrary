d5e3db65215c9a2d1b96b928ddadf3573dcfe321
levoncii23 <levoncii23@gmail.com>
2020-12-14 12:36:22 +0300

    kkk

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -2,4 +2,14 @@ from django.db import models
 
 class MyModelName(models.Model):
     
-    my_field_name = models.CharField(max_length=20, help_text="Enter field documentatiry")
+    my_field_name = models.CharField(max_length=20, help_text="Enter field documentation")
+
+
+    class Meta:
+        ordering = ["-my_field_name"]
+
+    def get_absolute_url(self):
+        return reverse('model-detail-view', args=[str(self.id)])
+
+    def __str__(self):
+        return self.field_name
